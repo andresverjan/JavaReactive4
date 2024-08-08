acef42c502f18486770d5a76feec357bf3693b42
mrestre <mrestre@bancolombia.com.co>
2024-08-08 08:40:51 -0500

    Se elimina variable concat del objeto persona

--- ConcatName/src/Persona/Persona.java
+++ ConcatName/src/Persona/Persona.java
@@ -4,30 +4,11 @@ public class Persona {
     private String name;
     private String lastName;
 
-    public String getConcat() {
-        return concat;
-    }
-
-    public void setConcat(String concat) {
-        this.concat = concat;
-    }
-
-    private String concat;
-
     public Persona(String name, String lastName) {
         this.name = name;
         this.lastName = lastName;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-
+    
     public String getName() {
         return name;
     }
