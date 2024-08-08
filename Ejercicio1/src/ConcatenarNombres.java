720cbbd04a9c279daa62aa69f0c6f94bc1431d0a
Nicolas Quintero <nquinte@bancolombia.com.co>
2024-08-08 08:33:25 -0500

    solucion error llave

--- Ejercicio1/src/ConcatenarNombres.java
+++ Ejercicio1/src/ConcatenarNombres.java
@@ -1,24 +1,21 @@
 import java.util.function.Function;
 
 public class ConcatenarNombres {
-
-
     public static Persona concatenar(Persona person){
         String newName = person.getNombre() + " " + person.getApellido();
         String newApellido = person.getApellido() + " " + person.getNombre();
         Persona newPerson= new Persona(newName,newApellido);
         return newPerson;
     }
-    }
-    public static Persona applyFunction(Persona persona, Function<Persona,Persona> function){
-        return function.apply(persona);
-    }
+public static Persona applyFunction(Persona persona, Function<Persona,Persona> function){
+    return function.apply(persona);
+}
 
-    public static void main(String[] args) {
-        Persona nicolas=new Persona("Nicolas","Quintero");
-        Persona newPerson=applyFunction(nicolas, ConcatenarNombres::concatenar);
-        System.out.println("name: "+newPerson.getNombre());
-        System.out.println("lastname: "+newPerson.getApellido());
+public static void main(String[] args) {
+    Persona nicolas=new Persona("Nicolas","Quintero");
+    Persona newPerson=applyFunction(nicolas, ConcatenarNombres::concatenar);
+    System.out.println("name: "+newPerson.getNombre());
+    System.out.println("lastname: "+newPerson.getApellido());
 
-    }
+}
 }
