3fc9399c39b79e2553d93ea7f6812d34bc514637
lizegira <lizegira@bancolombia.com.co>
2024-08-08 08:45:31 -0500

    Crear funciones

--- Actividad1/src/Main.java
+++ Actividad1/src/Main.java
@@ -1,12 +1,31 @@
+import java.util.function.Function;
+
 public class Main {
     public static void main(String[] args) {
 
-        Persona persona;
 
-        public static Persona concatenar(Persona persona){
+        Persona persona = new Persona("Paola","Giraldo");
 
-        }
+        Persona personaConcat = procesarNombres(persona, Main::concatenar);
 
+        System.out.println("Nombre: " + personaConcat.getNombre());
+        System.out.println("Apellido: " + personaConcat.getApellido());
 
     }
+
+
+    public static Persona concatenar(Persona persona){
+
+        String nombreApellido = persona.getNombre() + persona.getApellido();
+        String apellidoNombre = persona.getApellido() + persona.getNombre();
+
+        return new Persona(nombreApellido, apellidoNombre);
+
+    }
+
+    public static Persona procesarNombres(Persona persona, Function<Persona, Persona> function){
+        return function.apply(persona);
+    }
+
+
 }
--- Actividad1/src/Persona.java
+++ Actividad1/src/Persona.java
@@ -3,6 +3,11 @@ public class Persona {
     String nombre;
     String apellido;
 
+    public Persona(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
     public String getNombre() {
         return nombre;
     }
