67cc8b347263951ccf1b2d290df7c379ccc01067
lizegira <lizegira@bancolombia.com.co>
2024-08-08 08:24:22 -0500

    Crear clase Persona

---
+++ Actividad1/src/Persona.java
@@ -0,0 +1,22 @@
+public class Persona {
+
+    String nombre;
+    String apellido;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+}
