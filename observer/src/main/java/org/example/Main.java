5553d4b56fa0018a9dc87c8ddb9b6403ed5d9460
lizegira <lizegira@bancolombia.com.co>
2024-08-14 11:06:42 -0500

    Código de ejemplo

--- observer/src/main/java/org/example/Main.java
+++ observer/src/main/java/org/example/Main.java
@@ -1,11 +1,62 @@
 package org.example;
 
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Observable<Integer> observable = Observable.range(1, 5);
+
+        Observer<Integer> observer = new Observer<Integer>() {
+
+            @Override
+
+            public void onSubscribe(Disposable d) {
+
+                System.out.println("Subscribed");
+
+            }
+
+
+
+            @Override
+
+            public void onNext(Integer value) {
+
+                System.out.println("Received: " + value);
+
+                //factorial(value);
+
+            }
+
+
+
+            @Override
+
+            public void onError(Throwable e) {
+
+                System.err.println("Error: " + e.getMessage());
+
+            }
+
+
+
+            @Override
+
+            public void onComplete() {
+
+                System.out.println("Completed");
+
+            }
+
+        };
+
+// Suscribir el observable al observador
+
+        observable.subscribe(observer);
+
+        //observable.unsubscribe(observer);
+
     }
-
-
-
-
 }
