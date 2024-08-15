6e4dc0c478890f9f2c8024d5cbb870c2497dda90
Sebastián Chavarría <schavar@bancolombia.com.co>
2024-08-15 18:25:39 -0500

    Se sube actividad de conexión a la BD

---
+++ actividad4/src/main/java/com/valko/actividad4/Actividad4Application.java
@@ -0,0 +1,13 @@
+package com.valko.actividad4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Actividad4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Actividad4Application.class, args);
+	}
+
+}
---
+++ actividad4/src/test/java/com/valko/actividad4/Actividad4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valko.actividad4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Actividad4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
