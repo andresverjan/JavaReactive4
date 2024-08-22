0741cc01be2e10d30f61860b108976c2ea068c67
lizegira <lizegira@bancolombia.com.co>
2024-08-22 12:36:34 -0500

    Actividad propuesta

--- reactive/src/main/java/org/example/Main.java
+++ reactive/src/main/java/org/example/Main.java
@@ -5,57 +5,30 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
-/*
-        Mono<String> helloMono = Mono.just("Hello");
-        StepVerifier.create(helloMono)
-                .expectNext("Hello")
-                .expectComplete()
-                .verify();
+        Random random = new Random();
 
         System.out.println("MONO");
-        Mono<String> mono = Mono.just("Hola, Mundo!");        // Suscribirse al Mono y procesar el elemento emitido
-        mono.subscribe(
-                element -> System.out.println("Elemento recibido: " + element),
-                error -> System.err.println("Error: " + error.getMessage()),
-                () -> System.out.println("Secuencia completada"));
+        Mono<Integer> mono = Mono.just(random.nextInt(50));
 
-
-// Flux
-        System.out.println("FLUX");
-        Flux<String> stringFlux = Flux.just("Hello", "Baeldung");
-        StepVerifier.create(stringFlux)
-                .expectNext("Hello")
-                .expectNext("Baeldung")
-                .expectComplete()
-                .verify();
-
-        Flux<String> flux = Flux.just("Hola", "Mundo", "!");
-        flux.subscribe(
-                element -> System.out.println("Elemento recibido: " + element),
-                error -> System.err.println("Error: " + error.getMessage()),
-                () -> System.out.println("Secuencia completada")
-        );
-
-
-*/
-
-
-        System.out.println("MONO");
-        Mono<Integer> mono = Mono.just(1);
-
-        Disposable monoSuscriber1 = mono.subscribe(num -> System.out.println("Suscriber 1: Multiplica x 2 -> " + num * 2));
-        Disposable monoSuscriber2 = mono.subscribe(num -> System.out.println("Suscriber 2: Multiplica x 100 -> " + num * 100));
-        Disposable monoSuscriber3 = mono.subscribe(num -> System.out.println("Suscriber 3: Suma 50 -> " + num + 50));
+        mono.subscribe(num -> System.out.println("Entrada: " + num));
+        Disposable monoSuscriber1 = mono.filter(num -> num > 10).subscribe(num -> System.out.println("Suscriber 1: -> " + Math.sqrt(num)));
+        Disposable monoSuscriber2 = mono.filter(num -> num > 20).subscribe(num -> System.out.println("Suscriber 2: -> " + Math.sqrt(num)));
 
         System.out.println("FLUX");
-        Flux<Integer> flux = Flux.just(2,8,90,75,748,8,2356);
+        Flux<String> flux = Flux.just("Argentina", "Bolivia", "Brasil", "Chile", "Colombia", "Paraguay");
 
-        Disposable fluxSuscriber1 = flux.subscribe(num -> System.out.println("Suscriber 1: Multiplica x 2 -> " + num * 2));
-        Disposable fluxSuscriber2 = flux.subscribe(num -> System.out.println("Suscriber 2: Multiplica x 100 -> " + num * 100));
-        Disposable fluxSuscriber3 = flux.subscribe(num -> System.out.println("Suscriber 3: Suma 50 -> " + num + 50));
+        Disposable fluxSuscriber1 = flux.filter(country -> country.startsWith("C"))
+                .subscribe(country -> System.out.println("Suscriber 1: Empieza por C -> " + country));
+        Disposable fluxSuscriber2 = flux.filter(country -> country.length() > 5)
+                .subscribe(country -> System.out.println("Suscriber 2: Longitud > 5 -> " + country + " " + country.replaceAll("[^AEIOUaeiouÁÉÍÓÚáéíóú]","")));
+
+
+
 
 
     }
