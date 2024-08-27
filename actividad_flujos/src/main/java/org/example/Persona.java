f67ea6e6eb2b3f42dd512ed362c4b6bbe4f69252
lizegira <lizegira@bancolombia.com.co>
2024-08-27 09:57:48 -0500

    Crear proyecto actividad propuesta

---
+++ actividad_flujos/src/main/java/org/example/Persona.java
@@ -0,0 +1,51 @@
+package org.example;
+
+public class Persona {
+
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private int edad;
+    private String signo;
+
+
+    public Persona(String nombre, String apellido, String telefono, int edad, String signo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.edad = edad;
+        this.signo = signo;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getSigno() {
+        return signo;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", edad=" + edad +
+                ", signo='" + signo + '\'' +
+                '}';
+    }
+}
