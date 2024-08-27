238976bf541169ab9fd2284fd7babe9f4a1d3bc2
lizegira <lizegira@bancolombia.com.co>
2024-08-27 15:40:33 -0500

    Actividad de flujos reactivos

--- actividad_flujos/src/main/java/org/example/Main.java
+++ actividad_flujos/src/main/java/org/example/Main.java
@@ -1,30 +1,43 @@
 package org.example;
 
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class Main {
+
+    private static final List<Persona> listaPersonas = new ArrayList<>();
+
+    static {
+        listaPersonas.add(new Persona("Juan", "Pérez", "123456789", 30, "Aries"));
+        listaPersonas.add(new Persona("María", "Gómez", "987654321", 25, "Virgo"));
+        listaPersonas.add(new Persona("Carlos", "Martínez", "555444333", 40, "Capricornio"));
+        listaPersonas.add(new Persona("Laura", "Rodríguez", "111222333", 35, "Tauro"));
+        listaPersonas.add(new Persona("Pedro", "Sánchez", "999888777", 28, "Leo"));
+        listaPersonas.add(new Persona("Ana", "Fernández", "666777888", 22, "Acuario"));
+        listaPersonas.add(new Persona("David", "López", "333222111", 45, "Cáncer"));
+        listaPersonas.add(new Persona("Sofía", "Díaz", "777666555", 32, "Géminis"));
+        listaPersonas.add(new Persona("Javier", "Hernández", "888999000", 27, "Escorpio"));
+        listaPersonas.add(new Persona("Elena", "García", "112233445", 33, "Libra"));
+        listaPersonas.add(new Persona("Pablo", "Muñoz", "554433221", 38, "Piscis"));
+        listaPersonas.add(new Persona("Rosa", "Jiménez", "998877665", 29, "Sagitario"));
+    }
+
+
+    //Crear un Flux a partir de la lista de personas.
+    static Flux<Persona> fluxPersonas = Flux.fromIterable(listaPersonas);
+
     public static void main(String[] args) {
 
-        Persona persona1 = new Persona("Juan", "Pérez", "123456789", 30, "Aries");
-        Persona persona2 = new Persona("María", "Gómez", "987654321", 25, "Virgo");
-        Persona persona3 = new Persona("Carlos", "Martínez", "555444333", 40, "Capricornio");
-        Persona persona4 = new Persona("Laura", "Rodríguez", "111222333", 35, "Tauro");
-        Persona persona5 = new Persona("Pedro", "Sánchez", "999888777", 28, "Leo");
-        Persona persona6 = new Persona("Ana", "Fernández", "666777888", 22, "Acuario");
-        Persona persona7 = new Persona("David", "López", "333222111", 45, "Cáncer");
-        Persona persona8 = new Persona("Sofía", "Díaz", "777666555", 32, "Géminis");
-        Persona persona9 = new Persona("Javier", "Hernández", "888999000", 27, "Escorpio");
-        Persona persona10 = new Persona("Elena", "García", "112233445", 33, "Libra");
-        Persona persona11 = new Persona("Pablo", "Muñoz", "554433221", 38, "Piscis");
-        Persona persona12 = new Persona("Rosa", "Jiménez", "998877665", 29, "Sagitario");
 
-        //Crear un Flux a partir de la lista de personas.
-        Flux<Persona> fluxPersonas = Flux.just(persona1,persona2,persona3,persona4,persona5,persona6,persona7,persona8,persona9,
-                persona10,persona11,persona12);
+
+        System.out.println("===================================================================");
+        System.out.println("Crear un Flux a partir de la lista de personas.");
+        fluxPersonas.subscribe(System.out::println);
 
         System.out.println("===================================================================");
         System.out.println("Filtrar las personas mayores de 30 años utilizando filter().");
@@ -37,18 +50,85 @@ public class Main {
 
         System.out.println("===================================================================");
         System.out.println("Crear un Mono con la primera persona de la lista.");
-        Mono<Persona> primeraPersona = Mono.just(fluxPersonas.toStream().findFirst()).flatMap(Mono::justOrEmpty);
+        Mono<Persona> primeraPersona = Mono.just(listaPersonas.get(0));
+        primeraPersona.subscribe(System.out::println);
 
         System.out.println("===================================================================");
         System.out.println("Mostrar el nombre y apellido de la persona del Mono utilizando flatMap() y subscribe().");
-        primeraPersona.map(persona -> Stream.of(persona.getNombre(), persona.getApellido())).().subscribe(System.out::println);
+        //primeraPersona.map(persona -> List.of(persona.getNombre(), persona.getApellido())).subscribe(System.out::println);
+        primeraPersona.flatMap(persona -> Mono.just(persona.getNombre() + " " + persona.getApellido()))
+                .subscribe(System.out::println);
 
         System.out.println("===================================================================");
-        System.out.println("Agrupar a las personas por signo del zodiaco utilizando groupBy(), flatMap() y collectList(). Luego, mostrar el signo y la cantidad de personas para cada grupo.");
+        System.out.println("Agrupar a las personas por signo del zodiaco utilizando groupBy(), flatMap() y collectList(). " +
+                "Luego, mostrar el signo y la cantidad de personas para cada grupo.");
+        Mono<List<Long>> fluxSignos = fluxPersonas.groupBy(Persona::getSigno).flatMap(Flux::count).collectList();
 
-        //Flux<Persona> fluxSignos = fluxPersonas.groupBy(Persona::getSigno).flatMap(s -> s.collect());
+        System.out.println("===================================================================");
+        System.out.println("Crear una función obtenerPersonasPorEdad(int edad) que reciba una edad como parámetro" +
+                " y devuelva un Flux con las personas que tengan esa edad.");
+        Disposable personasPorEdad = obtenerPersonasPorEdad(30);
 
+        System.out.println("===================================================================");
+        System.out.println("Crear una función obtenerPersonasPorSigno(String signo) que reciba un signo del zodiaco " +
+                "como parámetro y devuelva un Flux con las personas que tengan ese signo.");
+        Disposable personasPorSigno = obtenerPersonasPorSigno("Aries");
+
+        System.out.println("===================================================================");
+        System.out.println("Crear una función obtenerPersonaPorTelefono(String telefono) que reciba un número " +
+                "de teléfono como parámetro y devuelva un Mono con la persona que tenga ese número de teléfono. " +
+                "Si no se encuentra, devolver un Mono vacío.");
+        Disposable personasPorTelefono = obtenerPersonasPorTelefono("554433221");
+
+        System.out.println("===================================================================");
+        System.out.println("Crear una función agregarPersona(Persona persona) que reciba una persona como parámetro " +
+                "y la agregue a la lista de personas. Devolver un Mono con la persona agregada.");
+        Persona nuevaPersona = new Persona("Paola", "Giraldo", "1112233344", 30, "Sagitario");
+        Disposable personaAgregada = agregarPersona(nuevaPersona);
+
+
+        System.out.println("===================================================================");
+        System.out.println("Crear una función eliminarPersona(Persona persona) que reciba una persona como parámetro " +
+                "y la elimine de la lista de personas. Devolver un Mono con la persona eliminada.");
+        Persona eliminarPersona = new Persona("Paola", "Giraldo", "1112233344", 30, "Sagitario");
+        Disposable personaEliminada = eliminarPersona(nuevaPersona);
 
 
     }
+
+
+    public static Disposable obtenerPersonasPorEdad(int edad) {
+        return fluxPersonas
+                .filter(persona -> persona.getEdad() == edad)
+                .collect(Collectors.toList()).flatMapMany(Flux::fromIterable)
+                .subscribe(System.out::println);
+    }
+
+    public static Disposable obtenerPersonasPorSigno(String signo) {
+        return fluxPersonas
+                .filter(persona -> persona.getSigno() == signo)
+                .collect(Collectors.toList()).flatMapMany(Flux::fromIterable)
+                .subscribe(System.out::println);
+    }
+
+    public static Disposable obtenerPersonasPorTelefono(String telefono) {
+        return fluxPersonas
+                .filter(persona -> persona.getTelefono() == telefono)
+                .collect(Collectors.toList()).flatMapMany(Flux::fromIterable).switchIfEmpty(Mono.empty())
+                .subscribe(System.out::println);
+    }
+
+    public static Disposable agregarPersona(Persona persona) {
+        listaPersonas.add(persona);
+        return Mono.just(persona).subscribe(System.out::println);
+    }
+
+    public static Disposable eliminarPersona(Persona persona) {
+        listaPersonas.remove(persona);
+        return Mono.just(persona).subscribe(System.out::println);
+    }
+
+
+
+
 }
