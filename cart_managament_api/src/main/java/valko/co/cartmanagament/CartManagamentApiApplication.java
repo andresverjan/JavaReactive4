08de87ee287a5f0b3e3cc5a901c3b3da75c88b4a
Edison Orozco <edorozc@bancolombia.com.co>
2024-09-13 17:11:00 -0500

    - Base carrito de compras

---
+++ cart_managament_api/src/main/java/valko/co/cartmanagament/CartManagamentApiApplication.java
@@ -0,0 +1,13 @@
+package valko.co.cartmanagament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartManagamentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartManagamentApiApplication.class, args);
+    }
+
+}
---
+++ cart_managament_api/src/test/java/valko/co/cartmanagament/CartManagamentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package valko.co.cartmanagament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartManagamentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
